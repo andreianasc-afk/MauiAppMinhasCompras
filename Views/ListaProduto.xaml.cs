01b8c68043d93ba4dac8579e3fb7bbc3e965228e
Andreia Nascimento <andreia.tulip@gmail.com>
2026-08-29 17:03:28 -0300

    Inserção de ObservableCollection e outras atualizações.

--- Views/ListaProduto.xaml.cs
+++ Views/ListaProduto.xaml.cs
@@ -1,12 +1,27 @@
+using MauiAppMinhasCompras.Models;
+using System.Collections.ObjectModel;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class ListaProduto : ContentPage
 {
+    ObservableCollection<Produto> Lista = new ObservableCollection<Produto>();
 	public ListaProduto()
 	{
 		InitializeComponent();
+
+        lst_produtos.ItemsSource = Lista;
 	}
 
+    protected async override void OnAppearing()
+    {
+
+        List<Produto> tmp = await App.Db.GetAll();
+
+        Lista.Clear();
+
+        tmp.ForEach(i => Lista.Add(i));
+    }
     private void ToolbarItem_Clicked(object sender, EventArgs e)
 	{
         try
@@ -21,6 +36,29 @@ public partial class ListaProduto : ContentPage
 
 
     }
-		
-			
+
+    private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        string q = e.NewTextValue;
+
+        Lista.Clear();
+
+        List<Produto> tmp = await App.Db.Search(q);
+
+        tmp.ForEach(i => Lista.Add(i));
+    }
+	
+    private void ToolbarItem_Clicked_1(Object sender, EventArgs e) 
+    {
+        double soma = Lista.Sum(i => i.Total);
+
+        string msg = $"O total é {soma:C}";
+
+        DisplayAlert("Total dos Produtos", msg, "OK");
+    }
+
+    private void MenuItem_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
